be809ee76b14d1f380daaf228fa98d27585ec7df
Tibirica Neto <tibirabotto@Tibiricas-MacBook-Pro.local>
2023-02-25 11:48:22 -0500

    Create pages Login and SignUP

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,57 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from "react";
+import { Text } from "react-native";
+import {
+  Container,
+  Title,
+  Input,
+  Button,
+  ButtonText,
+  SignUpButton,
+  SignUpText,
+} from "./styles";
 
 export default function Login() {
- return (
-    <View>
-        <Text>Login</Text>
-    </View>
+  const [login, setLogin] = useState(true);
+
+  function toggleLogin() {
+    setLogin(!login);
+  }
+
+
+  if (login) {
+    return (
+      <Container>
+        <Title>
+          Dev<Text style={{ color: "#E52246" }}>Post</Text>
+        </Title>
+        <Input placeholder="email@email.com" />
+        <Input placeholder="*****" secureTextEntry />
+        <Button onPress={() => console.log("Sign In")}>
+          <ButtonText>Sign In</ButtonText>
+        </Button>
+
+        <SignUpButton onPress={toggleLogin}>
+          <SignUpText>Create an account</SignUpText>
+        </SignUpButton>
+      </Container>
+    );
+  }
+
+  return (
+    <Container>
+      <Title>
+        Dev<Text style={{ color: "#E52246" }}>Post</Text>
+      </Title>
+      <Input placeholder="Name" />
+      <Input placeholder="email@email.com" />
+      <Input placeholder="*****" secureTextEntry />
+      <Button onPress={() => console.log("Sign In")}>
+        <ButtonText>Sign Up</ButtonText>
+      </Button>
+
+      <SignUpButton onPress={toggleLogin}>
+        <SignUpText>Already have an account?</SignUpText>
+      </SignUpButton>
+    </Container>
   );
 }
