d22967ee81681985d5f114bd8c589042e8d10cd4
Tibirica Neto <tibirabotto@Tibiricas-MacBook-Pro.local>
2023-02-25 11:08:12 -0500

    Created pages and Stack and Tabsbar

--- App.js
+++ App.js
@@ -1,53 +1,18 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import auth from "@react-native-firebase/auth";
+import { StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+
+import Routes from "./src/routes";
 
 export default function App() {
-  function handleLogin() {
-    auth()
-  .createUserWithEmailAndPassword('jane.doe@example.com', 'SuperSecretPassword!')
-  .then(() => {
-    console.log('User account created & signed in!');
-  })
-  .catch(error => {
-    if (error.code === 'auth/email-already-in-use') {
-      console.log('That email address is already in use!');
-    }
-
-    if (error.code === 'auth/invalid-email') {
-      console.log('That email address is invalid!');
-    }
-
-    console.error(error);
-  });
-  }
-
-  function handleLogout() {
-    auth()
-  .signOut()
-  .then(() => console.log('User signed out!'));
-  }
-
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <TouchableOpacity onPress={handleLogin}>
-        <Text>OK</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={handleLogout}>
-        <Text>Logout</Text>
-      </TouchableOpacity>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar
+        backgroundColor="#36393F"
+        barStyle="light-content"
+        translucent={false}
+      />
+      <Routes />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ src/routes/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { View, ActivityIndicator } from "react-native";
+
+import AuthRoutes from "./auth.routes";
+import AppRoutes from "./app.routes";
+
+function Routes() {
+  const signed = false;
+  const loading = false;
+
+  if(loading) {
+    <View
+      style={{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor: '#36393F'
+      }}
+    >
+      <ActivityIndicator size={50} color="#E52246" />
+    </View>
+  }
+
+  return (
+    signed ? <AppRoutes/> : <AuthRoutes/>
+  )
+}
+
+export default Routes;
