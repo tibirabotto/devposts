c2ad99383b5cb3b4c494e05d2e337eb4374b0520
Tibira Botto <tibira.botto@gmail.com>
2023-02-26 09:20:06 -0500

    Customize tab bar

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,22 +1,58 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import Home from '../pages/Home'
-import Profile from '../pages/Profile'
-import Search from '../pages/Search'
-import NewPost from '../pages/NewPost'
-import PostsUser from '../pages/PostsUser'
+import Feather from "react-native-vector-icons/Feather";
+
+import Home from "../pages/Home";
+import Profile from "../pages/Profile";
+import Search from "../pages/Search";
+import NewPost from "../pages/NewPost";
+import PostsUser from "../pages/PostsUser";
 
 const Tab = createBottomTabNavigator();
 
 function AppRoutes() {
   return (
-    <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home}  />
-        <Tab.Screen name="Search" component={Search}  />
-        <Tab.Screen name="Profile" component={Profile}  />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarKeyboardHidesTabBar: true,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#202225",
+          borderTopWidth: 0,
+        },
+        tabBarActiveTintColor: "#FFF",
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Feather name="home" color={color} size={size} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Feather name="search" color={color} size={size} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Feather name="user" color={color} size={size} />;
+          },
+        }}
+      />
     </Tab.Navigator>
-  )
+  );
 }
 
 export default AppRoutes;
