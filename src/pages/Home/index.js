e518481812227264d454acd33d92f04e2952e8d4
Tibira Botto <tibira.botto@gmail.com>
2023-02-27 11:15:03 -0500

    Create compoment Header and created Flatlist

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { useNavigation } from '@react-navigation/native'
-import Feather from 'react-native-vector-icons/Feather'
-import {Container, ButtonPost} from './styles'
+import React, { useState } from "react";
+import { Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import Feather from "react-native-vector-icons/Feather";
+import { Container, ButtonPost, ListPosts } from "./styles";
+
+import Header from "../../components/Header";
 
 export default function Home() {
-  const navigation = useNavigation()
- return (
+  const navigation = useNavigation();
+  const [posts, setPosts] = useState([
+    {id: '1', name: 'Tibira'},
+    {id: '2', name: 'Lucca'},
+    {id: '3', name: 'Gael'},
+    {id: '4', name: 'Conceicao'},
+  ]);
+  return (
     <Container>
-        <ButtonPost onPress={() => navigation.navigate('NewPost')}>
-          <Feather
-            name="edit-2"
-            color="#FFF"
-            size={25}
-          />
-        </ButtonPost>
+      <Header />
+      <ListPosts
+        data={posts}
+        renderItem={({item}) => (<Text>{item.name}</Text>)}
+      />
+      <ButtonPost onPress={() => navigation.navigate("NewPost")}>
+        <Feather name="edit-2" color="#FFF" size={25} />
+      </ButtonPost>
     </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -16,3 +16,9 @@ position: absolute;
 bottom: 6%;
 right: 6%;
 `;
+
+export const ListPosts = styled.FlatList`
+
+`;
+
+
